818bb166afec38bbac5a02c10d77a2db6b81c02f
David Lutta <davidlutta2010@gmail.com>
2020-01-19 21:06:14 +0300

    Finish static UI for home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mpesa/screens/HomePage.dart';
+import 'package:flutter_mpesa/bloc/CartListBloc.dart';
+import 'package:flutter_mpesa/screens/Home/Home.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,13 +9,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      blocs: [
+        Bloc((i) => CartListBloc()),
+      ],
+      child: MaterialApp(
+        title: 'Salad Delivery',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.lime,
+        ),
+        home: Home(),
       ),
-      home: HomePage(),
     );
   }
 }
