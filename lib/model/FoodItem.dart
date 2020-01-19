6bcb7cc1c81452f2b09fb78ca7f9bd0a290d17e9
David Lutta <davidlutta2010@gmail.com>
2020-01-19 16:32:58 +0300

    create model and dummy data

---
+++ lib/model/FoodItem.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+
+FoodItemList foodItemList = FoodItemList(foodItems: [
+  FoodItem(id: 1, title: "Tossed Salad", hotel: "SliceLime", price: 1.0, imageUrl: "https://www.weightwatchers.com/images/1033/dynamic/foodandrecipes/2016/02/TossedSaladWithClassicItalianDressing_MA16_EAT_FAMILY_D_800X800.jpg"),
+  FoodItem(id: 2, title: "Caesar Salad", hotel: "Cauto'de cuisine", price: 1.0, imageUrl: "https://www.onceuponachef.com/images/2010/08/Homemade-Caesar-Salad-Dressing-760x887.jpg"),
+  FoodItem(id: 3, title: "Chicken Salad", hotel: "Nandos", price: 1.0, imageUrl: "https://nutritiouslife.com/wp-content/uploads/2010/05/shutterstock_274855409-copy-1302x988.jpg"),
+  FoodItem(id: 3, title: "Chilean Salad", hotel: "Caffe Chile", price: 1.0, imageUrl: "https://therecipe.website/wp-content/uploads/2017/06/Chilean-Salad-Ensalada-Chilena-1-600x338.jpg"),
+  FoodItem(id: 4, title: "Cobb Salad", hotel: "Salad Bar", price: 1.0, imageUrl: "https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/1520887441-cobb-salad-delish-1.jpg?crop=1.0xw:1xh;center,top&resize=768:*"),
+  FoodItem(id: 5, title: "Coleslaw", hotel: "KFC", price: 1.0, imageUrl: "https://www.cookingclassy.com/wp-content/uploads/2018/06/coleslaw-recipe-2.jpg"),
+  FoodItem(id: 6, title: "Fruit Salad", hotel: "Java", price: 1.0, imageUrl: "https://hips.hearstapps.com/hmg-prod/images/fruit-salad-horizontal-jpg-1522181219.jpg"),
+  FoodItem(id: 7, title: "Potato Salad", hotel: "Beef Eaters", price: 1.0, imageUrl: "https://www.inspiredtaste.net/wp-content/uploads/2016/10/Easy-Potato-Salad-Recipe-2-1200.jpg"),
+  FoodItem(id: 8, title: "Garden Salad", hotel: "Garden Fresh", price: 1.0, imageUrl: "https://www.culinaryhill.com/wp-content/uploads/2016/05/Easy-Garden-Salad-Culinary-Hill-5-660x660.jpg"),
+  FoodItem(id: 9, title: "Greek Salad", hotel: "Salad Bar", price: 1.0, imageUrl: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/4/23/0/BX0204_greek-salad_s4x3.jpg.rend.hgtvcom.616.462.suffix/1529943050536.jpeg"),
+  FoodItem(id: 10, title: "Kachumbari", hotel: "Local Delis", price: 1.0, imageUrl: "https://www.preciouscore.com/wp-content/uploads/2019/02/Kachumbari-500x500.jpg")
+]);
+class FoodItemList{
+  List<FoodItem> foodItems;
+
+  FoodItemList({@required this.foodItems});
+
+}
+class FoodItem{
+  int id;
+  String title;
+  String hotel;
+  double price;
+  String imageUrl;
+  int quantity;
+
+  FoodItem({
+    @required this.id,
+    @required this.title,
+    @required this.hotel,
+    @required this.price,
+    @required this.imageUrl,
+    this.quantity = 1});
+
+  void incrementQuantity() {
+    this.quantity++;
+  }
+  void decrementQuantity() {
+    this.quantity--;
+  }
+}
