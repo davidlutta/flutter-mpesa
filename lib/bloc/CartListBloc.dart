73400c1e56776150cb6250e27054ed93bc2eb26f
David Lutta <davidlutta2010@gmail.com>
2020-01-19 16:47:52 +0300

    Create Bloc and Provider Classes

---
+++ lib/bloc/CartListBloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter_mpesa/bloc/CartProvider.dart';
+import 'package:flutter_mpesa/model/FoodItem.dart';
+import 'package:rxdart/rxdart.dart';
+
+class CartListBloc extends BlocBase {
+  CartListBloc();
+
+  CartProvider provider = CartProvider();
+
+  var _listController = BehaviorSubject<List<FoodItem>>.seeded([]);
+
+  // output
+  Stream<List<FoodItem>> get listStream => _listController.stream;
+
+  // input
+  Sink<List<FoodItem>> get listSink => _listController.sink;
+
+  addToList(FoodItem foodItem) {
+    listSink.add(provider.addToList(foodItem));
+  }
+
+  removeFromList(FoodItem foodItem) {
+    listSink.add(provider.removeFromList(foodItem));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _listController.close();
+  }
+}
